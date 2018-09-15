c627e1e95ab7e473d1127ed69d910cfa15c399da
Fred Z <freezed@users.noreply.github.com>
2018-09-15 11:54:35 +0200

    Logs down response #6

--- flasklocal/static/js/ask.js
+++ flasklocal/static/js/ask.js
@@ -4,21 +4,23 @@ function addSubmit(ev) {
     fetch('/ask', {method: 'POST', body: new FormData(this)})
     .then(function(response) { return response.json(); })
     .then(function(json) {
-        var chatDiv = document.getElementById('chat');
-
+        const chatElt = document.getElementById('chat');
+        answerElt = document.createElement('div');
         const markup = `
                 <p>${json.question}</p>
                 <p>${json.address}</p>
                 <p>
                     ${json.extract}
                     Mais je fatigue : c'est l'heure de la sièste!
-                    Va donc voir <a href='https://fr.wikipedia.org/w/index.php?curid=${json.curid}'>sur wikipedia</a>, pendant que je vais faire un somme (-:
+                    Va donc voir <a href='https://fr.wikipedia.org/w/index.php?curid=${json.curid}'>sur wikipedia</a>.
                 </p>
                 <p><a href="${json.map_link}"><img src="${json.map_img_src}"></a></p>
             `
-        chatDiv.innerHTML = markup;
+        answerElt.innerHTML = markup;
+        chatElt.appendChild(answerElt)
+        chatElt.innerHTML += '\n            '
    })
 };
 
-var form = document.getElementById('form');
+const form = document.getElementById('form');
 form.addEventListener('submit', addSubmit);
